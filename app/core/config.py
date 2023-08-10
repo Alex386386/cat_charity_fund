4c9a5f7cc810398b6989aecc93a7f8f8b44d5c67
Alex386386 <mamonov40600@mail.ru>
2023-08-10 10:00:06 +0300

    first review #0.4

--- app/core/config.py
+++ app/core/config.py
@@ -7,8 +7,8 @@ class Settings(BaseSettings):
     app_title: str = 'Приложение QRKot'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
-    first_superuser_email: Optional[EmailStr] = 'FIRST_SUPERUSER_EMAIL'
-    first_superuser_password: Optional[str] = 'FIRST_SUPERUSER_PASSWORD'
+    first_superuser_email: Optional[EmailStr] = 'user@example.com'
+    first_superuser_password: Optional[str] = '1234566789987654321'
 
     class Config:
         env_file = '.env'
